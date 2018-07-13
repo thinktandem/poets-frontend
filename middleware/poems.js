96636f5cb4affe8d0e7ad3009abfbd5daa013a8b
Geoff St. Pierre <serundeputy@gmail.com>
2018-07-13 08:09:30 -0400

    Code cleanup.

--- middleware/poems.js
+++ middleware/poems.js
@@ -9,7 +9,6 @@ export default function({params, store}) {
   const apiUrl = process.env.API_URL;
 
   return axios.get(apiUrl + `/node/poem/${params.id}`)
-    //`http://poetscontentapi.lndo.site/api/tutorials/${params.id}`) //938c0e11-a6d8-48f5-8c34-06c7574cf81f')
     .then((response) => {
       store.commit('addPoem', response.data);
     });
