2d8d23cd3455242526d01c664f5174e0f589370a
Geoff St. Pierre <serundeputy@gmail.com>
2018-10-05 13:33:30 -0400

    #46: Adding AppAwards, AppAnnouncements, AppAnnouncementsAwards components.

---
+++ components/AppAnnouncementsAwards/AppAnnouncements.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="bg-white p-4">
+    <h2>Announcements</h2>
+    <div
+      v-for="announcement in announcements"
+      :key="announcement.link"
+      class="announcement">
+      <div class="announcement-date">
+        {{ announcement.date }}
+      </div>
+      <div class="announcement-title">
+        {{ announcement.title }}
+      </div>
+      <div class="announcement-read-more">
+        <a href="announcemnt.link">read more</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  /**
+   * @todo replace with real data/prop
+   * @return {{ announcements: {date: string, title: string, link: string}[]}}
+   */
+  data() {
+    return {
+      announcements: [
+        {
+          date: "Aug 11, 2018",
+          title: "A Letter from the Executive Director",
+          link: "https://appUrl/announcementId"
+        },
+        {
+          date: "Jul 31, 2018",
+          title: "The Academy of American Poets Is Pleased to",
+          link: "https://appUrl/announcementId"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style scoped>
+.announcement {
+  padding: 9px;
+  border-bottom: 1px #ccc solid;
+}
+.announcement-date {
+  color: red;
+}
+</style>
---
+++ components/AppAnnouncementsAwards/AppAnnouncementsAwards.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-white p-4">
+    <b-container class="mx-auto my-1">
+      <div class="row">
+        <div class="col-8">
+          <AppAnnouncements />
+        </div>
+        <div class="col">
+          <AppAwards />
+        </div>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import AppAnnouncements from "~/components/AppAnnouncementsAwards/AppAnnouncements";
+import AppAwards from "~/components/AppAnnouncementsAwards/AppAwards";
+
+export default {
+  components: {
+    AppAnnouncements,
+    AppAwards
+  }
+};
+</script>
+<style scoped>
+.promo__disclosure {
+  color: var(--dark-muted);
+  font-weight: 400;
+}
+</style>
