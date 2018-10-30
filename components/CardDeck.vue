b2694a4ab99a9b38bf1faf0487dda438d85dc2ba
Josh <josh.walker@tithe.ly>
2018-10-30 14:31:45 -0700

    #63: Stubbed out card deck component

---
+++ components/CardDeck.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="card-deck container">
+    <b-row>
+      <h3>{{ title }}</h3>
+      <nuxt-link to="{{ link.href }}">{{ link.title }}</nuxt-link>
+    </b-row>
+    <b-row>
+      <div
+        v-for="card in cards"
+        :key="card.id"
+        class="card-deck__card {{ card.classes }}">
+      >
+      {{ card }}
+      </div>
+    </b-row>
+  </section>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
