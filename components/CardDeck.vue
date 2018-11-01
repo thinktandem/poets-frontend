7a2e0eb886a5f3c236d862d41fc66659b9872714
Josh <josh.walker@tithe.ly>
2018-11-01 14:37:10 -0700

    New idea for deck component

--- components/CardDeck.vue
+++ components/CardDeck.vue
@@ -1,28 +1,30 @@
 <template>
-  <section class="card-deck container">
+  <b-card-group class="card-deck container"> deck
     <b-row>
       <h3>{{ title }}</h3>
       <nuxt-link to="{{ link.href }}">{{ link.title }}</nuxt-link>
     </b-row>
     <b-row>
-      <div
+      <b-card
         v-for="card in cards"
         :key="card.id"
         class="card-deck__card {{ card.classes }}">
       >
-      {{ card }}
-      </div>
+
+      <component :is="cardType" v-bind="props"></component>
+
+      </b-card>
     </b-row>
-  </section>
+  </b-card-group>
 </template>
 
 <script>
 
 export default {
+  props: ['title', 'link', 'cards']
   components: {
   },
   data() {
-
   }
 };
 </script>
