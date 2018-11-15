1d832d784b80d3254346b87c40d94ecfde3fa753
Josh <josh.walker@tithe.ly>
2018-11-14 17:33:26 -0800

    #62: Adding temp style guide page, fixing the poet component and implementing the poem deck

---
+++ pages/styleguide-temp.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <CardDeck
+      title="Poets"
+      cardtype="Poet"
+      :cards="poets"
+      :link="link"/>
+    <CardDeck
+      title="Poems"
+      cardtype="PoemCard"
+      :cards="poems"
+      :link="link"/>
+
+  </div>
+</template>
+
+<script>
+import PoemCard from "~/components/Poems/PoemCard";
+import Poet from "~/components/Poet";
+import CardDeck from "~/components/CardDeck";
+export default {
+  layout: "default",
+  components: {
+    PoemCard,
+    CardDeck,
+    Poet
+  },
+  data() {
+    return {
+      poems: [
+        {
+          title: "A Poem About a Thing",
+          text: `There once was a man from Nantucket
+Who kept all his cash in a bucket.
+  But his daughter, named Nan,
+  Ran away with a man
+And as for the bucket, Nantucket.`,
+          poet: {
+            name: "Dustin LeKensrue"
+          },
+          year: "2018"
+        },
+        {
+          title: "A Poem About a Thing",
+          text: `There once was a man from Nantucket
+Who kept all his cash in a bucket.
+  But his daughter, named Nan,
+  Ran away with a man
+And as for the bucket, Nantucket.`,
+          poet: {
+            name: "Dustin LeKensrue"
+          },
+          year: "2018"
+        },
+        {
+          title: "A Poem About a Thing",
+          text: `There once was a man from Nantucket
+Who kept all his cash in a bucket.
+  But his daughter, named Nan,
+  Ran away with a man
+And as for the bucket, Nantucket.`,
+          poet: {
+            name: "Dustin LeKensrue"
+          },
+          year: "2018"
+        }
+      ],
+      poets: [
+        {
+          name: "Dustin LeKensrue",
+          bio: "Praesent commodo cursus magna, vel scelerisque nisl consect",
+          img: {
+            src: "/images/poet.png"
+          },
+          link: {
+            href: "blah/blah"
+          }
+        },
+        {
+          name: "Dustin LeKensrue",
+          bio: "Praesent commodo cursus magna, vel scelerisque nisl consect",
+          img: {
+            src: "/images/poet.png"
+          },
+          link: {
+            href: "blah/blah"
+          }
+        },
+        {
+          name: "Dustin LeKensrue",
+          bio: "Praesent commodo cursus magna, vel scelerisque nisl consect",
+          img: {
+            src: "/images/poet.png"
+          },
+          link: {
+            href: "blah/blah"
+          }
+        }
+      ],
+
+      link: {
+        href: "blah/blah",
+        text: "More Poems"
+      }
+    };
+  }
+};
+</script>
+
+<style>
+</style>
