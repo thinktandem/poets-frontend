edfb0f8124062596b5de9926d9fab41e0764212e
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2018-11-16 16:42:31 -0500

    Add another basic test

--- test/home_test.js
+++ test/home_test.js
@@ -4,3 +4,8 @@ Scenario("Ad section is available on homepage", I => {
   I.amOnPage("/");
   I.seeElement(".promo__image");
 });
+
+Scenario("Poem-a-day is available on homepage", I => {
+  I.amOnPage("/");
+  I.seeElement(".poem-a-day-container");
+})
