86c22472cc76960967103f8c1c7e7e041d8e5cf7
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2018-11-20 15:00:37 -0500

    Issue #94: Add @nuxtjs/dotenv

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 module.exports = {
   modules: [
-    //  '@nuxtjs/axios',
+    "@nuxtjs/dotenv",
     ["bootstrap-vue/nuxt", { css: false }],
     //  'vue-youtube-embed'
     [
