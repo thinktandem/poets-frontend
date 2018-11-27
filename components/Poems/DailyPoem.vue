2460f1a9a76fe328deb91dd601943f6f9e207b6b
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2018-11-27 13:34:51 -0500

    Issue #35: Pull Poem of the Day from Drupal
    This includes the logic needed to fetch the title and body of the poem
    of the day from Drupal. We still need to handle a lot of the logic
    around pulling the reference to the author. I've stubbed the right parts
    of the api call here but we also need to fetch and update the store for
    the rest of that data when it gets populated in Drupal.

--- components/Poems/DailyPoem.vue
+++ components/Poems/DailyPoem.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="poem-a-day-container pt-4">
-    <div
-      v-for="poem in poems"
-      :key="poem.link"
-      class="poem-a-day__poems">
+    <div class="poem-a-day__poems">
       <h3 class="poem-a-day-container__poet-name p-2">
-        {{ poem.poet }}
+        {{ poet.name }}
       </h3>
       <b-container>
         <div class="row">
           <div class="col-4 poem-a-day-container__poet-image">
-            <b-img
-              :src="poem.image"
-            />
+            <b-img :src="poet.image"/>
             <div class="poem-a-day-container__sign-up-form">
               <AppPoemADaySignUpForm />
             </div>
@@ -56,134 +51,36 @@
 <script>
 import AppPoemADaySignUpForm from "~/components/AppPoemADayPoems/AppPoemADaySignUpForm";
 import AppPoems from "~/components/AppPoemADayPoems/AppPoems";
-
 export default {
+  name: "DailyPoem",
   components: {
     AppPoemADaySignUpForm,
     AppPoems
   },
-  /**
-   * @todo replace with real data/prop
-   * @return {{ announcements: {date: string, title: string, link: string}[]}}
-   */
-  data() {
-    let poemTitle =
-      "It Was Summer Now and the Colored People Came Out Into the Sunshine";
-    let poemText = `
-      <p>
-      Oh, my planet, how beautiful
-      you are. Little curve that leads me
-      to the lakeside. Let me step out
-      </p>
-      <p>
-      of the sack of skin I wore
-      on earth. It’s good to be home.
-      No more need to name me. No more
-      </p>
-      need to make the shape of a machete
-      with my mouth. Pushing up up up the tired
-      sides that want to drop below my teeth.
-      </p>
-      <p>
-      Lord, I’ve missed you. The streets
-      covered all day in light from the moons.
-      I was confused all the time. I wanted so much.
-      </p>
-      <p>
-      My hole felt like a gut with an antler
-      rammed through it. So lonely and strange
-      and always trying to smile. Coin of the realm.
-      </p>
-      <p>
-      And my arms open and my life
-      coming in and out of the “ATM.”
-      Once I saw a fox leap inside the morning
-      </p>
-      <p>
-      light and made the same shape
-      of myself. Once I watched the boats
-      and also rocked back and forth.
-      </p>
-      <p>
-      How does every person not cry out
-      all the time? Yes, it was good to eat
-      doughnuts. Yes. I was blessed by many
-      </p>
-      <p>
-      days of joy. A rabbit in the driveway.
-      A rosemary bush with a sorcerer’s cloak
-      of spider webs. Brian Eno.
-      </p>
-      <p>
-      The Hammond B3 Organ that never asked
-      me who I knew. But that body.
-      Like a factory. That mind like a ship
-      </p>
-      <p>
-      built to pile in other bodies. Skin like a
-      sow without any of the sow’s equanimity.
-      It reflected nothing. Pink skin. Blue eyes
-      </p>
-      <p>
-      hard as an anvil. Like a window with covering
-      that refuses the passerby’s gaze. I loved
-      the bully power some days. Oh my pleasure
-      </p>
-      <p>
-      in not causing harm. My pride. I’m not like
-      so-and-so. My pink skin preaching, my pink skin
-      yawping out my other hole, “I did not choke
-      </p>
-      <p>
-      the man with my elbow!” “Would never!”
-      “I let all the boys in hoodies walk
-      through dark streets.” “I did not shoot
-      </p>
-      <p>
-      them with my guns!” The ship rising
-      up inside me. As if the fox felt pride
-      for not tearing the bird to pieces. As if
-      </p>
-      <p>
-      the owl’s heart grew large from not
-      wrecking the squirrel’s nest. My pink skin
-      a sail full of indignation. My eyes pitching
-      </p>
-      <p>
-      across the feed. It is so good to be home
-      and yet. I have a ship inside. How can
-      the organ welcome me? I’m not a sow
-      </p>
-      <p>
-      on her worst day. Which would be what?
-      Breaking from the barn? Eating all the acorns
-      and rolling in the mud? No.
-      </p>
-      <p>
-      Her worst would be at my hands
-      and on my plate for supper. Grow
-      like the tree, the man who heals
-      </p>
-      <p>
-      the bodies said. In every way I became
-      the ship rising in the harbor.
-      How can I be welcomed after that?
-      </p>
-`;
-    return {
-      poems: [
-        {
-          poet: "Gabrielle Calvocoressi",
-          image: "/images/gc.png",
-          link: "https://appUrl/announcementId",
-          title: poemTitle,
-          text: poemText
-        }
-      ]
-    };
+  props: {
+    poem: {
+      type: Object,
+      default() {
+        return {
+          text: "",
+          title: ""
+        };
+      }
+    },
+    poet: {
+      type: Object,
+      default() {
+        return {
+          name: "",
+          image: "",
+          link: ""
+        };
+      }
+    }
   }
 };
 </script>
+
 <style scoped lang="scss">
 .poem-a-day-container {
   border-top: 4px var(--white) solid;
@@ -247,6 +144,7 @@ export default {
           rgba(255, 255, 255, 1)
         );
       }
+
       .poem-a-day-container__read-the-rest {
         width: 146px;
         color: var(--white);
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,71 @@
 <template>
   <div>
-    <AppPoemADayPoemsContainer />
+    <daily-poem
+      :poem="$store.state.poemOfTheDay.poem"
+      :poet="$store.state.poemOfTheDay.poet"/>
     <AppAnnouncementsAwards />
   </div>
 </template>
 
 <script>
-import AppPoemADayPoemsContainer from "~/components/AppPoemADayPoems/AppPoemADayPoemsContainer";
 import AppAnnouncementsAwards from "~/components/AppAnnouncementsAwards/AppAnnouncementsAwards";
+import DailyPoem from "../components/Poems/DailyPoem";
+import * as moment from "moment";
+import _ from "lodash";
 export default {
   layout: "default",
   components: {
-    AppPoemADayPoemsContainer,
+    DailyPoem,
     AppAnnouncementsAwards
   },
-  fetch({ store, params }) {
+  async fetch({ app, store, params }) {
+    // Fetch all poems with poem a day date somewhere today.
+    const dateFilter = {
+      condition: {
+        path: "field_poem_of_the_day_date",
+        operator: "BETWEEN",
+        value: [
+          moment()
+            .hour(0)
+            .minute(0)
+            .second(0)
+            .unix(),
+          moment()
+            .hour(23)
+            .minute(59)
+            .second(59)
+            .unix()
+        ]
+      }
+    };
+    const response = await app.$axios.$get("/api/node/poems", {
+      params: {
+        _format: "json",
+        filter: {
+          date: dateFilter
+        },
+        fields: {
+          "node--poems": "title,body,field_author,field_soundcloud_embed_code"
+        },
+        includes: "field_author"
+      }
+    });
+    if (response.data.length < 0) {
+      const poem = _.first(response.data);
+      // @todo: when we fix the author field, put a real author data pull in here.
+      // Do the same with the link, we need to get a resource URL for the front end for the poem.
+      store.commit("updatePoemOfTheDay", {
+        poet: {
+          name: "Gabrielle Calvocoressi",
+          image: "/images/gc.png"
+        },
+        poem: {
+          link: "https://appUrl/announcementId",
+          title: poem.attributes.title,
+          text: poem.attributes.body.processed
+        }
+      });
+    }
     // Set the current hero
     store.commit("updateHero", {
       variant: "quote",
