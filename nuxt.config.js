6ff57d437534f39196d43ccf29100dec85de0d3f
Geoff St. Pierre <serundeputy@gmail.com>
2018-11-28 12:24:57 -0500

    thinktandem/poets-api#135: Remove dotenv.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,6 @@
-// require("dotenv").config();
-
 module.exports = {
   modules: [
     "@nuxtjs/axios",
-    //  "@nuxtjs/dotenv",
     ["bootstrap-vue/nuxt", { css: false }],
     // 'vue-youtube-embed'
     [
