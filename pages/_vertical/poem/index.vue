34b309afbf9100d44daa78ba2745e67b17f40b45
Geoff St. Pierre <serundeputy@gmail.com>
2018-12-15 11:59:40 -0500

    thinktandem/poets-api#209: Adding /poetsorg/poems page.

--- pages/_vertical/poem/index.vue
+++ pages/_vertical/poem/index.vue
@@ -1,68 +1,162 @@
 <template>
   <div>
-    <b-container class="poems-list py-5">
-      <b-row class="poems-list__row">
+    <b-container class="poems-list__filters filters">
+      <b-row class="poems-list__filters-row">
+        <b-col md="12">
+          <b-form
+            class="poems-list__search"
+            @submit.stop.prevent="applyFilters"
+          >
+            <b-form-group>
+              <div class="legend-selects">
+                <div class="poems-list__filters__legend">
+                  <legend>Filter by</legend>
+                </div>
+              </div>
+              <div class="poems-list__input--search">
+                <b-form-input
+                  v-model="combinedInput"
+                  type="text"
+                  size="22"
+                  placeholder="Search title or text ..."
+                />
+                <b-btn class="btn-primary">
+                  <iconSearch />
+                </b-btn>
+              </div>
+            </b-form-group>
+          </b-form>
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-container class="poems-list tabular-list">
+      <b-row class="tabular-list__row tabular-list__header">
         <b-col md="4">
-          <div class="poems-list__list-header">
-            Name
-          </div>
+          Name
         </b-col>
         <b-col md="4">
-          <div class="poems-list__list-header">
-            Years
-          </div>
+          Years
         </b-col>
         <b-col md="4">
-          <div class="poems-list__list-header">
-            Schools and Movements
-          </div>
+          Schools and Movements
         </b-col>
       </b-row>
       <b-row
-        v-for="poem in poems"
-        class="poems-list__row poems-list__poems"
-        :key="poem.id">
-        <b-col md="4">
-          <a :href="`/poems/${getPoemTitle(poem.view_node)}`">
-            {{ poem.title }}
-          </a>
-        </b-col>
-        <b-col md="4">
-          {{ poem.nothing }}
-        </b-col>
-        <b-col md="4">
-          {{ poem.field_school_movement }}
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col md="4">
-          <a :href="`/poems?page=${Prev}`">
-            &lt;&lt; Prev
-          </a>
-        </b-col>
-        <b-col md="4">
-          <a href="/poems?page=1">
-            1
-          </a>
-          <a href="/poems?page=2">
-            2
-          </a>
-          <a href="/poems?page=3">
-            3
-          </a>
-          . . .
-          <a :href="`/poems?page=${$store.state.numPages.numPages}`">
-            {{ $store.state.numPages.numPages }}
-          </a>
-        </b-col>
+        v-for="poem in results"
+        class="tabular-list__row poems-list__poems"
+        :key="poem.id"
+      >
         <b-col md="4">
           <a
-            v-if="$store.state.numPages.numPages > Next"
-            :href="`/poems?page=${Next}`">
-            Next &gt;&gt;
-          </a>
+            :href="poem.view_node"
+            v-html="poem.title"
+          />
+        </b-col>
+        <b-col md="4">
+          {{ poem.field_author }}
+        </b-col>
+        <b-col md="4">
+          {{ poem.field_date_published }}
         </b-col>
       </b-row>
+      <div class="pager">
+        <ul
+          role="menubar"
+          aria-disabled="false"
+          aria-label="Pagination"
+          class="pagination"
+        >
+          <li
+            role="none presentation"
+            aria-hidden="true"
+            class="page-item"
+            :class="{ disabled: !currentPage}"
+          >
+            <a
+              :href="`/poetsorg/poem?page=${Prev}${preparedCombine}`"
+              class="page-link"
+            >
+              <iconMediaSkipBackwards /> Prev
+            </a>
+          </li>
+          <li
+            role="none presentation"
+            aria-hidden="true"
+            class="page-item"
+          >
+            <a
+              v-if="pageNum + 1 < totalPages"
+              :href="`/poetsorg/poem?page=${pageNum + 1}{preparedCombine}`"
+              class="page-link"
+            >
+              {{ pageNum + 1 }}
+            </a>
+
+          </li>
+          <li
+            role="none presentation"
+            aria-hidden="true"
+            class="page-item"
+          >
+            <a
+              v-if="pageNum + 2 < totalPages"
+              :href="`/poetsorg/poem?page=${pageNum + 2}${preparedCombine}`"
+              class="page-link"
+            >
+              {{ pageNum + 2 }}
+            </a>
+          </li>
+
+          <li
+            role="none presentation"
+            aria-hidden="true"
+            class="page-item"
+          >
+            <a
+              v-if="pageNum + 3 < totalPages"
+              :href="`/poetsorg/poem?page=${pageNum + 3}${preparedCombine}`"
+              class="page-link"
+            >
+              {{ pageNum + 3 }}
+            </a>
+          </li>
+          <li
+            role="none presentation"
+            aria-hidden="true"
+            class="page-item ellipsis"
+          >
+            <span>&hellip;</span>
+          </li>
+          <li
+            role="none presentation"
+            aria-hidden="true"
+            class="page-item"
+          >
+            <a
+              v-if="pageNum + 1 < totalPages"
+              :href="`/poetsorg/poem?page=${totalPages - 1}${preparedCombine}`"
+              class="page-link"
+            >
+              {{ totalPages }}
+            </a>
+          </li>
+          <li
+            role="none presentation"
+            aria-hidden="true"
+            class="page-item"
+          >
+            <a
+              :href="`/poetsorg/poem?page=${Next}${preparedCombine}`"
+              class="page-link"
+              :class="{disabled: !Next}"
+            >
+              Next
+              <iconMediaSkipForwards />
+            </a>
+
+          </li>
+        </ul>
+      </div>
     </b-container>
   </div>
 </template>
@@ -70,24 +164,143 @@
 <script>
 import AppPoemADaySignUpForm from "~/components/AppPoemADayPoems/AppPoemADaySignUpForm";
 import AppPoems from "~/components/AppPoemADayPoems/AppPoems";
+import searchHelpers from "~/plugins/search-helpers";
+import iconMediaSkipBackwards from "~/static/icons/media-skip-backwards.svg";
+import iconMediaSkipForwards from "~/static/icons/media-skip-forwards.svg";
+import iconSearch from "~/static/icons/magnifying-glass.svg";
+
 export default {
-  components: { AppPoemADaySignUpForm, AppPoems },
-  async asyncData({ app, params, query }) {},
-  async fetch({ app, store, params }) {},
-  methods: {}
+  components: {
+    AppPoemADaySignUpForm,
+    AppPoems,
+    iconMediaSkipBackwards,
+    iconMediaSkipForwards,
+    iconSearch
+  },
+  data() {
+    return {
+      combinedInput: null,
+      results: null,
+      Next: null,
+      Prev: null,
+      preparedCombine: null
+    };
+  },
+  async asyncData({ app, params, query }) {
+    const url = "/api/poems";
+    return searchHelpers.getSearchResults(url, app, query);
+  },
+  methods: {
+    applyFilters() {
+      let myQuery = {};
+      if (this.combinedInput) {
+        myQuery.combine = this.combinedInput;
+      }
+      this.$router.push({
+        name: "vertical-poem",
+        query: myQuery
+      });
+    }
+  },
+  watchQuery: true
 };
 </script>
 
 <style scoped lang="scss">
-.poems-list__row {
-  border-bottom: 1px solid #ccc;
-  font-size: 18px;
-  min-height: 47px;
-}
-.poems-list__list-header {
-  font-weight: bold;
-}
 .poems-list__poems {
-  font-weight: 111;
+  font-weight: 400;
+  a {
+    color: $body-color;
+
+    &:hover,
+    &:focus,
+    &:active {
+      text-decoration: underline;
+    }
+  }
+}
+
+.poems-list {
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+}
+
+.poems-list__search {
+  margin-top: 2rem;
+}
+
+.legend-selects {
+  display: flex;
+  flex-basis: 100%;
+  padding: 1rem 1rem 1rem 2rem;
+  border-right: $form__border;
+
+  select {
+    &:not(:last-child) {
+      margin-right: 1rem;
+    }
+  }
+}
+
+.poems-list__filters__legend {
+  flex-basis: 50%;
+
+  legend {
+    margin: 0;
+    line-height: 2;
+    font-size: $font-size-base;
+    color: $gray-700;
+  }
+}
+
+.poems-list__input--search {
+  flex-basis: 100%;
+  padding: 1rem;
+  position: relative;
+
+  input {
+    border: none;
+    background-color: transparent;
+
+    &:hover,
+    &:focus,
+    &:active {
+      border: none;
+      background-color: transparent;
+    }
+
+    &::placeholder {
+      color: $gray-700;
+    }
+  }
+
+  button {
+    width: 2rem;
+    height: 2rem;
+    display: flex;
+    padding: 0;
+    justify-content: center;
+    position: absolute;
+    top: 50%;
+    right: 1rem;
+    transform: translateY(-50%);
+    background-color: transparent;
+    border: none;
+
+    &:hover,
+    &:focus,
+    &:active,
+    &:active:focus {
+      // Some really sticky rules getting applied from BS that need a bit of
+      // force.
+      background-color: transparent !important;
+      box-shadow: none !important;
+    }
+
+    svg {
+      width: 100%;
+      height: 100%;
+    }
+  }
 }
 </style>
