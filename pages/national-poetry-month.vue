ae73055d7c1831601372da236717ab3dc4e4b332
Geoff St. Pierre <serundeputy@gmail.com>
2019-01-02 11:38:55 -0500

    Adding twitter integration infra ... still needs some css  paint.

--- pages/national-poetry-month.vue
+++ pages/national-poetry-month.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
-    <b-container class="poems-list__filters filters">
-      <b-row class="poems-list__filters-row">
+    <b-container class="npm__container">
+      <b-row>
         <b-col md="12">
-          tweets ...
-          <br>
-          {{ tweets }} .... gff is the best?
+          Twitter :: Poets.org
+        </b-col>
+      </b-row>
+      <b-row class="npm__tweets-row">
+        <b-col
+          v-for="tweet in tweets"
+          :key="`--${tweet}`"
+          md="3"
+          class="npm__tweets-col"
+        >
+          <div class="npm__tweets-col-date">
+            <a :href="tweet.created_at">
+              {{ tweet.created_at }}
+            </a>
+          </div>
+          <div class="npm__tweets-col-text">
+            {{ tweet.text }}
+          </div>
         </b-col>
       </b-row>
     </b-container>
@@ -13,8 +28,6 @@
 </template>
 
 <script>
-// import qs from "querystring";
-
 export default {
   components: {},
   data() {
@@ -22,28 +35,29 @@ export default {
       tweets: {}
     };
   },
-  asyncData({ app, params, query }) {
+  async asyncData({ app, params, query }) {
     const Twit = require("twit");
     const config = {
       consumer_key: "VD1mP2OGqooLrjxrHWBDQHUbo",
       consumer_secret: "IYnXGS44hYcqLG4RLm3gGL7yFlqz1t3BjWob4UW1Yyyb7OmtGx",
       access_token: "17496303-FpXOkS3TpEWIk9zqR2EmWkNEICWWm3Qn3Onjx8N4X",
       access_token_secret: "QnOByeSXPDSdOX7zT5E6JpydMCNldHr2IDDxrHFWAo0mx"
-      // callBackUrl: "/national-poetry-month"
     };
     const T = new Twit(config);
-    // https://api.twitter.com/1.1/statuses/user_timeline.json
-    return T.get(
-      "/statuses/user_timeline",
-      { screen_name: "POETSorg", count: 4 },
-      (err, data, newRes) => {
-        console.log("\n\ngff----------------------\n\n\n", data[0]);
-        return {
-          tweets: data
-        };
-      }
-    ).catch(err => {
-      console.log(err);
+
+    /*
+     * To use promises API w/ T do not use a callback to T.get.
+     *
+     * https://api.twitter.com/1.1/statuses/user_timeline.json
+     */
+    return T.get("statuses/user_timeline", {
+      screen_name: "POETSorg",
+      count: 4
+    }).then(res => {
+      console.log(res.data);
+      return {
+        tweets: res.data
+      };
     });
   },
   methods: {},
@@ -52,4 +66,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.npm__tweets-col {
+  font-weight: 400;
+}
 </style>
