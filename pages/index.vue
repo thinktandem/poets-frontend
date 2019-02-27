74b948d69c6aa435741d74a8bb94715e0219c140
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2019-02-27 12:58:40 -0500

    Issue #141: Add About Poem information

--- pages/index.vue
+++ pages/index.vue
@@ -72,7 +72,8 @@ export default {
         title: theOnePoemOfTheDay.poem.title,
         text: theOnePoemOfTheDay.poem.text,
         soundCloud: theOnePoemOfTheDay.poem.soundcloud,
-        alias: theOnePoemOfTheDay.poem.alias
+        alias: theOnePoemOfTheDay.poem.alias,
+        about: _.get(theOnePoemOfTheDay, "poem.about", null)
       }
     });
 
