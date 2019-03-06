9f9e2c951f60c2c0fdb1276bc76a9323c0831fa7
Stephanie Fuda <stephanie0042@gmail.com>
2019-03-06 12:29:58 -0500

    284: getting subprize data correct in the return and display

--- pages/_vertical/prizes/_title.vue
+++ pages/_vertical/prizes/_title.vue
@@ -3,52 +3,202 @@
     <b-container class="py-5">
       <b-row>
         <b-col xl="12">
-          <h1>{{ prizes.title }}</h1>
+          <h1>{{ prize.title }}</h1>
         </b-col>
       </b-row>
       <b-row>
         <b-col
-          v-html="prizes.body"
+          v-html="prize.body"
           class="prizes__body"
           xl="12"/>
       </b-row>
+    </b-container>
+
+    <b-container class="prizes-list tabular-list">
+      <b-row class="tabular-list__row tabular-list__header">
+        <b-col
+          md="2">
+          Year
+        </b-col>
+        <b-col md="3">
+          Poet
+        </b-col>
+        <b-col md="7">
+          Title
+        </b-col>
+      </b-row>
+      <b-row
+        v-for="(subPrize, index) in subPrizes"
+        class="tabular-list__row prizes-list__books"
+        :key="index"
+      >
+        <b-col
+          class="date"
+          md="2"
+        >
+          {{ subPrize.date }}
+        </b-col>
+        <b-col
+          class="books-list__books-title"
+          md="3">
+          <b-link 
+            :to="subPrize.winnerLink" 
+            class="text-dark">
+            {{ subPrize.name }}
+        </b-link></b-col>
+        <b-col md="7">
+          <b-link 
+            :to="subPrize.winningLink" 
+            class="text-dark">
+            {{ subPrize.title }}
+          </b-link>
+        </b-col>
+      </b-row>
+      <div class="pager">
+        <ul
+          role="menubar"
+          aria-disabled="false"
+          aria-label="Pagination"
+          class="pagination"
+        >
+          <li
+            role="none presentation"
+            aria-hidden="true"
+            class="page-item"
+            :class="{ disabled: !currentPage}"
+          >
+            <a
+              :href="`/poetsorg/prizes?page=${Prev}${preparedCombine}`"
+              class="page-link"
+            >
+              <iconMediaSkipBackwards /> Prev
+            </a>
+          </li>
+          <li
+            role="none presentation"
+            aria-hidden="true"
+            class="page-item"
+          >
+            <a
+              v-if="pageNum + 1 < totalPages"
+              :href="`/poetsorg/prizes?page=${pageNum + 1}{preparedCombine}`"
+              class="page-link"
+            >
+              {{ pageNum + 1 }}
+            </a>
+
+          </li>
+          <li
+            role="none presentation"
+            aria-hidden="true"
+            class="page-item"
+          >
+            <a
+              v-if="pageNum + 2 < totalPages"
+              :href="`/poetsorg/prizes?page=${pageNum + 2}${preparedCombine}`"
+              class="page-link"
+            >
+              {{ pageNum + 2 }}
+            </a>
+          </li>
+
+          <li
+            role="none presentation"
+            aria-hidden="true"
+            class="page-item"
+          >
+            <a
+              v-if="pageNum + 3 < totalPages"
+              :href="`/poetsorg/prizes?page=${pageNum + 3}${preparedCombine}`"
+              class="page-link"
+            >
+              {{ pageNum + 3 }}
+            </a>
+          </li>
+          <li
+            role="none presentation"
+            aria-hidden="true"
+            class="page-item ellipsis"
+          >
+            <span>&hellip;</span>
+          </li>
+          <li
+            role="none presentation"
+            aria-hidden="true"
+            class="page-item"
+          >
+            <a
+              v-if="pageNum + 1 < totalPages"
+              :href="`/poetsorg/prizes?page=${totalPages - 1}${preparedCombine}`"
+              class="page-link"
+            >
+              {{ totalPages }}
+            </a>
+          </li>
+          <li
+            role="none presentation"
+            aria-hidden="true"
+            class="page-item"
+          >
+            <a
+              :href="`/poetsorg/prizes?page=${Next}${preparedCombine}`"
+              class="page-link"
+              :class="{disabled: !Next}"
+            >
+              Next
+              <iconMediaSkipForwards />
+            </a>
+
+          </li>
+        </ul>
+      </div>
     </b-container>
   </div>
 </template>
 
 <script>
-import * as qs from "qs";
-import * as _ from "lodash";
+import _ from "lodash";
 
 export default {
-  async asyncData({ app, params, query }) {
-    const prizes = await app.$axios
-      .$get(`/api/node/${}/${response.data.entity.uuid}`)
-      .then(response => {
-        return {
-          response: response,
-          data: {
-            title: "Prizes",
-            link: {
-              to: `/${params.vertical}/prizes`,
-              text: `${response.meta.count} Prizes`
-            },
-            prizes: _.map(response.data, item => {
-              return {
-                title: item.attributes.title,
-                titleLink: item.attributes.path.alias,
-                body: item.attributes.body.value
-              };
-            })
-          }
-        };
-      });
+  async asyncData({ app, route }) {
+    const routerResponse = await app.$axios.$get(
+      `/router/translate-path?path=${route.path}`
+    );
+    const prize = await app.$axios.$get(routerResponse.jsonapi.individual);
 
-    return { prizes: prizes };
+    const subPrize = await app.$axios.$get(
+      `/api/node/sub_prize_or_program?filter[field_parent.id]=${
+        prize.data.id
+      }&include=field_winner&sort=-created`
+    );
+
+    return {
+      subPrizes: _.map(subPrize.data, item => {
+        const winner = _.find(
+          subPrize.included,
+          include =>
+            _.get(include, "id") ===
+            _.get(_.first(_.get(item, "relationships.field_winner.data")), "id")
+        );
+
+        return {
+          title: _.get(item, "attributes.title"),
+          date: _.get(item, "attributes.created").split("-")[0],
+          winningLink: _.get(item, "attributes.path.alias"),
+          name: _.get(winner, "attributes.title"),
+          winnerLink: _.get(winner, "attributes.path.alias")
+        };
+      }),
+      prize: {
+        response: prize,
+        title: _.get(prize, "data.attributes.title"),
+        body: _.get(prize, "data.attributes.body.processed")
+      }
+    };
   }
 };
 </script>
-s
+
 <style scoped lang="scss">
 .book__body {
   font-weight: 400;
