5ff26e92ee058249d9fecc68e06f9b2e9e4a53e4
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2019-03-11 09:43:18 -0400

    Update components/AppNewsletterSupportFollow/AppFollow.vue

--- components/AppNewsletterSupportFollow/AppFollow.vue
+++ components/AppNewsletterSupportFollow/AppFollow.vue
@@ -64,7 +64,7 @@ export default {
           icon: "InstagramIcon"
         },
         {
-          url: "https://www.pinterest.com/",
+          url: "https://www.pinterest.com/poetsorg/",
           icon: "PinterestIcon"
         }
       ];
