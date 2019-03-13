92ed3e4cedc778c629dc84984cdbd955d645287d
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2019-03-13 14:42:23 -0400

    Issue #310, #340: Create abstract list page

---
+++ pages/academy-american-poets/american-poets-prizes.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <basic-page
+      :body="$store.state.pageData.data.attributes.body"
+      :extended-content="$store.state.extendedContent"
+      :highlighted="$store.state.highlightedData"
+      :more="$store.state.relatedContent"
+      :sidebar-data="$store.state.sidebarData"/>
+    <app-listing
+      resource-type="prize_or_program"
+      :includes="includes"
+      :fields="fields"/>
+  </div>
+</template>
+
+<script>
+import AppListing from "~/components/AppListing";
+import BasicPage from "~/components/BasicPage";
+
+export default {
+  components: {
+    AppListing,
+    BasicPage
+  },
+  data() {
+    return {
+      includes: {},
+      fields: {
+        title: { label: "Name" }
+      }
+    };
+  },
+  async fetch({ app, store, route }) {
+    return app.$buildBasicPage(app, store, route.path);
+  }
+};
+</script>
