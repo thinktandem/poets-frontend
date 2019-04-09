8db374352843f63e45c33f18544ef467e0df8e59
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2019-04-09 09:04:13 -0400

    Issue #543: Preserve query parameters through redirects

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -1,19 +1,24 @@
+import qs from "qs";
+import { isEmpty } from "lodash";
 import redirects from "~/redirects.json";
 /**
  * @param {Object} context
  *  the nuxt context, see https://nuxtjs.org/api/context
  * @return {mixed}.
  */
-export default function({ redirect, route }) {
+export default function({ redirect, route, query }) {
   const thisRedirect = redirects.find(r => r.from === route.path);
   const poetsorgPattern = RegExp("/poetsorg/");
   const homePattern = RegExp("/home");
+  // Handle query params so they don't get stripped
+  const paramString = isEmpty(query) ? "" : `?${qs.stringify(query)}`;
+
   if (thisRedirect) {
     console.log(`redirect: ${redirect.from} => ${redirect.to}`);
-    return redirect(thisRedirect.to);
+    return redirect(thisRedirect.to + paramString);
   } else if (poetsorgPattern.test(route.path)) {
-    return redirect(route.path.replace("/poetsorg/", "/"));
+    return redirect(route.path.replace("/poetsorg/", "/") + paramString);
   } else if (homePattern.test(route.path)) {
-    return redirect(route.path.replace("/home", "/"));
+    return redirect(route.path.replace("/home", "/") + paramString);
   }
 }
