d06fdf1b90fa126c7d041aeda99b95cdf2341e58
Alec Reynolds <alec+git@thinktandem.io>
2019-04-15 19:14:00 -0700

    #601: Use entire link as the dropdown trigger; add 'home' links to dropdown submenu.

--- components/AppMobileMenu.vue
+++ components/AppMobileMenu.vue
@@ -7,13 +7,19 @@
       <b-link
         v-b-toggle.nav_collapse
         class="bg-dark text-white"
-        :to="item.to">{{ item.text }}</b-link>
-      <a
+        v-if="!item.children"
+        :to="item.to">{{ item.text }}
+      </b-link>
+      <span
+        class="bg-dark text-white"
         v-if="item.children"
-        @click="item.open = !item.open">
+        @click="item.open = !item.open"
+        disabled>{{ item.text }}
         <drop-caret
           :class="{open: item.open}"
-          class="text-white caret"/></a>
+          class="text-white caret"/>
+      </span>
+
       <b-list-group
         class="border-top border-bottom border-primary py-3 sub-menu"
         v-show="item.open">
@@ -24,6 +30,7 @@
           <b-link
             v-b-toggle.nav_collapse
             class="text-white"
+            @click="item.open = false"
             :to="child.to">
             {{ child.text }}
           </b-link>
@@ -53,6 +60,12 @@ export default {
 <style lang="scss" scoped>
 .sub-menu {
   transition: all 0.5s;
+  .list-group-item {
+    font-weight: 200;
+  }
+  .list-group-item:first-child {
+    font-weight: bold;
+  }
 }
 .caret {
   transition: transform 0.1s;
