20ed7144f42a2724596f6d87f806521dfe6dea97
Mike Pirog <mike@thinktandem.io>
2019-04-24 22:52:35 -0400

    #763: NETWORK ERROR DEBUG 5

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,28 +1,7 @@
-import _ from "lodash";
-import qs from "qs";
-
 /**
  * Set the parameter serialization scheme for axios.
  * @param {Object} $axios
  * @param {Object} app
  * @param {Object} store
  */
-export default function({ $axios, redirect }) {
-  $axios.onRequest(config => {
-    config.headers.common["X-Consumer-ID"] = process.env.CONSUMER_ID;
-    config.paramsSerializer = params =>
-      qs.stringify(params, { arrayFormat: "brackets", encode: false });
-    return config;
-  });
-  $axios.onError(error => {
-    console.error(error);
-    console.table(error);
-    throw error.response;
-  });
-  $axios.onResponse(response => {
-    // Handle decoupled router redirects.
-    if (_.get(response, "data.redirect")) {
-      redirect(_.get(_.first(_.get(response, "data.redirect")), "to"));
-    }
-  });
-}
+export default function({ $axios, redirect }) {}
