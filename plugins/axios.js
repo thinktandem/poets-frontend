97e366304de3e12b13922d5ce3a2660f682381b0
Mike Pirog <mike@thinktandem.io>
2019-04-24 22:53:04 -0400

    #763: RESET DEBUGGING

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,23 @@
+import _ from "lodash";
+import qs from "qs";
+
 /**
  * Set the parameter serialization scheme for axios.
  * @param {Object} $axios
  * @param {Object} app
  * @param {Object} store
  */
-export default function({ $axios, redirect }) {}
+export default function({ $axios, redirect }) {
+  $axios.onRequest(config => {
+    config.headers.common["X-Consumer-ID"] = process.env.CONSUMER_ID;
+    config.paramsSerializer = params =>
+      qs.stringify(params, { arrayFormat: "brackets", encode: false });
+    return config;
+  });
+  $axios.onResponse(response => {
+    // Handle decoupled router redirects.
+    if (_.get(response, "data.redirect")) {
+      redirect(_.get(_.first(_.get(response, "data.redirect")), "to"));
+    }
+  });
+}
