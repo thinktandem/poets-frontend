a949210626fe15a4886e2c4a4c381a5f851a64fa
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2019-05-01 10:56:13 -0400

    Issue thinktandem/poets-api#764: Remove await

--- pages/node/_id.vue
+++ pages/node/_id.vue
@@ -1,29 +1,26 @@
-<template>
-  <div/>
-</template>
 <script>
-import _ from "lodash";
+import { get } from "lodash";
 
 export default {
   async asyncData({ app, router, params, redirect }) {
-    const whereTo = await app.$axios
+    return app.$axios
       .$get(`/router/translate-path?path=/node/${params.id}`)
-      .then(res => {
-        const path = app.$axios
+      .catch(err => {
+        app.handleError(err);
+      })
+      .then(res =>
+        app.$axios
           .$get(
-            `/api/` +
-              `${_.get(res, "entity.type")}/` +
-              `${_.get(res, "entity.bundle")}/` +
-              `${_.get(res, "entity.uuid")}`
+            `/api/${get(res, "entity.type")}/${get(res, "entity.bundle")}/${get(
+              res,
+              "entity.uuid"
+            )}`
           )
           .then(res => {
-            return _.get(res, "data.attributes.path.alias");
+            return redirect(get(res, "data.attributes.path.alias"));
           })
-          .catch(err => console.log(err));
-        return path;
-      })
-      .catch(err => console.log(err));
-    redirect(whereTo);
+          .catch(err => app.handleError(err))
+      );
   }
 };
 </script>
