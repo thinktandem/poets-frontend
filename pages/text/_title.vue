d48aec8516a9d6538b8f86fdec89eb49f0b62f05
Geoff St. Pierre <serundeputy@gmail.com>
2019-05-02 15:10:25 -0400

    thinktandem/poets-api#758: Adding author, type, and published date info to sidebar.

--- pages/text/_title.vue
+++ pages/text/_title.vue
@@ -4,16 +4,40 @@
       <b-row>
         <b-col xl="8">
           <h1>{{ text.attributes.title }}</h1>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col
-          class="text__body"
-          xl="8"
-        >
           <div
-            class="text__body-preface"
-            v-html="replaceFileUrl(body)"/>
+            class="text__body"
+            xl="8"
+          >
+            <div
+              class="text__body-preface"
+              v-html="replaceFileUrl(body)"/>
+          </div>
+        </b-col>
+        <b-col md="4">
+          <div class="text__contributors-label">
+            Author:
+          </div>
+          <div
+            v-if="contributors"
+            class="text__sidebar-item text__written-by"
+            v-for="(contributor, i) in contributors"
+            :key="i">
+            <b-link :to="contributor.attributes.path.alias">
+              {{ contributor.attributes.title }}
+            </b-link>
+          </div>
+          <div class="text__posted-label">
+            Posted:
+          </div>
+          <div class="text__sidebar-item text__date-published">
+            {{ datePublished }}
+          </div>
+          <div class="text__type-label">
+            Type:
+          </div>
+          <div class="text__sidebar-item text__type">
+            {{ type }}
+          </div>
         </b-col>
       </b-row>
     </b-container>
@@ -21,7 +45,9 @@
 </template>
 
 <script>
+import _ from "lodash";
 import imgUrl from "~/plugins/inlineImagesUrl.js";
+import niceDate from "~/plugins/niceDate";
 
 export default {
   async asyncData({ app, params }) {
@@ -36,9 +62,12 @@ export default {
       })
       .then(res => {
         return app.$axios
-          .get(`/api/node/texts/${res.data.entity.uuid}`)
+          .get(
+            `/api/node/texts/${res.data.entity.uuid}` +
+              `?include=field_contributors,field_texttype`
+          )
           .then(res => {
-            return res.data.data;
+            return res.data;
           })
           .catch(error => {
             console.log(error);
@@ -47,11 +76,28 @@ export default {
       .catch(error => {
         console.error(error);
       });
-    const body = await imgUrl.staticUrl(text.attributes.body.value, app);
+    const body = await imgUrl.staticUrl(text.data.attributes.body.value, app);
+    const contributors = [];
+    _.each(_.get(text, "included"), (include, i) => {
+      if (include.type === "node--person") {
+        contributors[i] = include;
+      }
+    });
+    const datePublished = niceDate.niceDate(
+      _.get(text, "data.attributes.field_date_published")
+    );
+    const type = _.find(_.get(text, "included"), include => {
+      if (include.type === "taxonomy_term--text_type") {
+        return include;
+      }
+    });
 
     return {
-      text,
-      body
+      text: text.data,
+      body,
+      contributors,
+      datePublished,
+      type: _.get(type, "attributes.name")
     };
   }
 };
@@ -68,5 +114,9 @@ export default {
     float: left;
     padding-right: 22px;
   }
+}
+.text__sidebar-item {
+  margin-bottom: 2rem;
+  font-weight: 400;
 }
 </style>
