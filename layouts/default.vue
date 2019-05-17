4c8e52b7825a1d89e69ecd9a6215d39be21ecaf5
John Ouellet <john@thinktandem.io>
2019-05-17 12:43:24 -0400

    #827 Fix up PageSpeed Scores (#483)
    * #827 Add in font preloaded, fix gpt, increase static asset maxage
    
    * #827 Adjust maxage
    
    * #827 Adjust maxage again
    
    * #827 Add in prefetch
    
    * Revert "#827 Add in prefetch"
    
    This reverts commit 2d02959de133c8d5f305e3e3f6d0f84edc656725.
    
    * #827 Add in preconnects

--- layouts/default.vue
+++ layouts/default.vue
@@ -46,9 +46,27 @@ export default {
     return {
       script: [
         {
-          src: "https://www.googletagservices.com/tag/js/gpt.js"
+          src: "https://www.googletagservices.com/tag/js/gpt.js",
+          defer: true
         },
         GptAds.gatherMeData().gptInitScript
+      ],
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://pixel.yabidos.com",
+          crossorigin: "true"
+        },
+        {
+          rel: "preconnect",
+          href: "https://www.google-analytics.com",
+          crossorigin: "true"
+        },
+        {
+          rel: "preconnect",
+          href: "https://pre.glotgrx.com",
+          crossorigin: "true"
+        }
       ]
     };
   },
