fcffa6da2605ea86f867c5877e38c6f83156c6ac
Geoff St. Pierre <serundeputy@gmail.com>
2019-06-21 07:24:44 -0400

    thinktandem/poets-api#898: Add GA tracking for pagination and filters on /poems.

--- pages/poems/index.vue
+++ pages/poems/index.vue
@@ -319,6 +319,22 @@ export default {
       });
       // Grab the movement and featured poets
       Promise.all([this.getTermDescription(), this.getFeaturedPoems()]);
+      let pageString = `/poems?page=${query.page}`;
+      if (!_.isEmpty(query.combine)) {
+        pageString += `&combine=${query.combine}`;
+      }
+      if (query.field_occasion_target_id) {
+        pageString += `&field_occasion_tid=${query.field_occasion_target_id}`;
+      }
+      if (query.field_poem_themes_target_id) {
+        pageString += `&field_poem_themes_tid=${
+          query.field_poem_themes_target_id
+        }`;
+      }
+      if (query.field_form_target_id) {
+        pageString += `&field_form_tid=${query.field_form_target_id}`;
+      }
+      this.$ga.page(pageString);
     },
     getFeaturedPoems() {
       const query = buildFeaturesPoemsQuery(this.filters);
