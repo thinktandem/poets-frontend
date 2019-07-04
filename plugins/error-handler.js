94c97b19d4c891701541e854a7beb99bbae013aa
John Ouellet <john@thinktandem.io>
2019-07-04 17:34:59 -0400

    thinktandem/poets#912 Add in proper status codes to 403/404 redirects

--- plugins/error-handler.js
+++ plugins/error-handler.js
@@ -3,10 +3,10 @@ export default ({ app, redirect }, inject) => {
   app.handleError = error => {
     switch (_.get(error, "response.status")) {
       case 403:
-        redirect("/403");
+        redirect([403], "/403");
         break;
       case 404:
-        redirect("/404");
+        redirect([404], "/404");
       default:
         redirect("/sorry");
     }
