d64c3d5c49016f2943e16f68eded6d629b76183c
Geoff St. Pierre <serundeputy@gmail.com>
2019-07-17 12:47:31 -0400

    thinktandem/poets-api#923 adding h1 to /texts/_author routes.

--- pages/texts/_author.vue
+++ pages/texts/_author.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <b-container md="8">
-      <h3 class="texts-by__title">
+      <h1 class="texts-by__title">
         {{ poet.data.attributes.title }}
-      </h3>
+      </h1>
     </b-container>
     <b-container class="table-container">
       <b-table
